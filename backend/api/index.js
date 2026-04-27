ae88051541245674f9ff25c496eed41018d44f16
Vi2507ki <vigneshviki25072003@gmail.com>
2026-04-27 11:37:12 +0530

    Fixed package.json and deployment issue

--- backend/api/index.js
+++ backend/api/index.js
@@ -4,14 +4,17 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-app.use(cors({
-  origin: "*"
-}));
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("MongoDB error:", err));
 
@@ -24,4 +27,8 @@ app.get("/", (req, res) => {
 // app.use("/api/auth", require("../routes/authRoutes"));
 // app.use("/api/products", require("../routes/productRoutes"));
 
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`TechMart server running on port ${PORT}`);
+});
